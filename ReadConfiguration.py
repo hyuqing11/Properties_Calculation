152389ba78ec037200a5ba0a9872ae1d0248eeea
hyuqing11 <yuiqinghuang2018@gmail.com>
2024-06-24 18:44:05 -0400

    test read vasp file

--- ReadConfiguration.py
+++ ReadConfiguration.py
@@ -1,14 +1,15 @@
 import numpy as np
 from typing import Tuple, Dict, List
-
+from MathFunctions import MathFunctions
 
 class ReadConfiguration:
-    def __init__(self, filename: str, num_frame: int, num_types: int, num_atoms: int, dim: int):
+    def __init__(self, filename: str,parameters):
         self.filename = filename
-        self.num_frame = num_frame
-        self.num_types = num_types
-        self.num_atoms = num_atoms
-        self.dim = dim
+        self.parameters = parameters
+        self.num_frame = parameters['num_frame']
+        self.num_types = parameters['num_types']
+        self.num_atoms = parameters['num_atoms']
+        self.dim = parameters['dim']
 
     def read_lammps(self) -> Tuple[
         np.ndarray, np.ndarray, np.ndarray, Dict[int, List[List[np.ndarray]]], Dict[int, List[List[np.ndarray]]]]:
@@ -56,3 +57,74 @@ class ReadConfiguration:
                     atom_vel_dict[atom_type][-1].append(vel[frame, atom])
 
         return pos, vel, latt_matrix, atom_pos_dict, atom_vel_dict
+
+    def displacement(self,data1,data2):
+        dr = np.subtract(data1, data2)
+        dr = dr - np.round(dr)
+        return dr
+    def compute_velocity(self,pos, vel, latt_matrix, atom_pos_dict, atom_vel_dict,,elements,atom_counts):
+        dr_car = np.zeros((self.num_frame - 1, self.num_atoms, 3))
+        dr = np.zeros((self.num_frame - 1,self.num_atoms, 3))
+        if len(latt_matrix) > 1:
+            print(
+                "Warning: The lattice constant changes over time, and the velocity calculation will be computed based on the previous time crystal framework")
+            npt = 1
+        else:
+            npt = 0
+        for j in range(1,self.num_frame):
+            latt = latt_matrix[j - 1] if npt else latt_matrix[0]
+            trans = np.transpose(
+                np.dot(np.transpose(np.linalg.inv(latt)),
+                       (np.dot(np.transpose(latt), np.transpose(pos[j])))))
+            dr[j - 1] = self.displacement(trans, pos[j - 1])
+            dr_car[j - 1] = np.transpose(np.dot(np.transpose(latt), np.transpose(dr[j - 1])))
+            vel[j - 1] = dr_car[j - 1] / self.parameters['dt']
+            for i in range(len(elements)):
+                end_i = start_i + atom_counts[i]
+                atom_vel_dict[i + 1].append(vel[j-1, start_i:end_i])
+                start_i = end_i
+        #if len(latt_matrix) == 1:
+
+    def read_vasp(self,sysName,compute_velocity):
+        with open(self.filename,'r') as f:
+            contcar = f.readlines()
+        pos = np.zeros((self.num_frame,self.num_atoms,self.dim))
+        vel = np.zeros((self.num_frame,self.num_atoms,self.dim))
+        elements = contcar[5].strip().split()
+        atom_counts = list(map(int,contcar[6].strip().split()))
+        atom_pos_dict = {i: [] for i in range(1,len(elements)+1)}
+        atom_vel_dict = {i: [] for i in range(1,len(elements)+1)}
+        latt_matrix = []
+        n = 0
+
+        for line_i, line in enumerate(contcar):
+            if sysName in line:
+                matrix1 = self.convert_lines_to_data(contcar[line_i + 2: line_i + 2 + self.dim])
+                latt_matrix.append(matrix1)
+            if "Direct" in line:
+                pos[n] = self.convert_lines_to_data(contcar[line_i + 1: line_i + 1 + self.num_atoms ])
+                start_i = 0
+                for i in range(len(elements)):
+                    end_i = start_i  + atom_counts[i]
+                    atom_pos_dict[i+1].append(pos[n, start_i:end_i])
+                    start_i = end_i
+                n += 1
+                if n + 1 > self.num_frame:
+                    break
+        if compute_velocity:
+            self.compute_velocity(pos, vel, latt_matrix, atom_pos_dict, atom_vel_dict,elements,atom_counts)
+        return pos, vel, latt_matrix, atom_pos_dict, atom_vel_dict
+
+
+    def convert_lines_to_data(self,lines):
+        data = []
+        for line in lines:
+            tem = line.split()
+            row = list(map(float, tem))
+            data.append(row)
+        return data
+
+    def _skip_lines(self, file, num_lines):
+        for _ in range(num_lines):
+            next(file)
+
